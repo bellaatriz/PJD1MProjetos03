c0e3984f900ac11b099676a3f258262e3c199a7c
bellaatriz <belatriz4157@gmail.com>
2025-01-14 08:28:15 -0300

    Projetos03

---
+++ Meu projeto PJD1M/Assets/Sprits/Player.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int numero = 10;
+    
+    void Start()
+    {
+        Debug.Log("Teste..." + numero);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
