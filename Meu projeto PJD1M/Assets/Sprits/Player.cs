deef9f9f10435dbe9ca21db089813a02acd6b424
bellaatriz <belatriz4157@gmail.com>
2025-01-23 11:52:14 -0300

    meu projeto23/01

--- Meu projeto PJD1M/Assets/Sprits/Player.cs
+++ Meu projeto PJD1M/Assets/Sprits/Player.cs
@@ -4,18 +4,33 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float velocidade = 10f;
+    public float focapulo = 10f;
 
-    public int numero = 10;
-    
+      public bool noChao = false;
+      
+      private Rigidbody2D _rigidbody2D
+      private SpriteRenderer SpriteRenderer;
+      
+    // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Teste..." + numero);
+        _rigidbody2D = 
+            gameObject.GetComponent<Rigidbody2D>();
+        SpriteRenderer = 
+            gameObject.GetComponent<SpriteRenderer>();
     }
 
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "chao")
+        {
+            noChao = false;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.LeftArrow))
     }
 }
