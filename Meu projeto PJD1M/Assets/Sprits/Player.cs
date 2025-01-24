9950d5905244f9b63c117165ebe357412b828b91
bellaatriz <belatriz4157@gmail.com>
2025-01-24 12:08:55 -0300

    Meu projeto24/01

--- Meu projeto PJD1M/Assets/Sprits/Player.cs
+++ Meu projeto PJD1M/Assets/Sprits/Player.cs
@@ -9,16 +9,15 @@ public class Player : MonoBehaviour
 
       public bool noChao = false;
       
-      private Rigidbody2D _rigidbody2D
-      private SpriteRenderer SpriteRenderer;
+      private Rigidbody2D _rigidbody2D;
+      private SpriteRenderer _spriteRenderer;
       
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody2D = 
             gameObject.GetComponent<Rigidbody2D>();
-        SpriteRenderer = 
-            gameObject.GetComponent<SpriteRenderer>();
+        _spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
     }
 
     void OnCollisionStay2D(Collision2D collision)
@@ -31,6 +30,20 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            gameObject.transform.position += new Vector3(-velocidade * Time.deltaTime, 0, 0);
+            //rigidbody2D.AddForce(nem Vector2(-velocidade,0));
+            _spriteRenderer.flipX = true;
+            Debug.Log("LeftArrow");
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.transform.position += new Vector3(velocidade * Time.deltaTime, 0, 0);
+            //rigidbody2D.AddForce(nem Vector2(velocidade,0));
+            _spriteRenderer.flipX = false;
+            Debug.Log("RightArrow");
+        }   
     }
 }
